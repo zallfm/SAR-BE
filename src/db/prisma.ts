81fcc4c2e3b85cb0ef33e3c43c1ae4cbee05bf57
andigagaga <andigagaga1@gmail.com>
2025-11-19 15:13:39 +0700

    generate db prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -2,12 +2,16 @@ import { PrismaClient as PrismaSAR } from "../generated/prisma/index.js";
 import { PrismaClient as PrismaSC } from "../generated/prisma-sc/index.js";
 import { PrismaClient as PrismaLdap } from "../generated/prisma-ldap/index.js";
 import { PrismaClient as PrismaTmmin } from "../generated/prisma-tmmin/index.js";
+import { PrismaClient as PrismaDataMaster } from "../generated/prisma-datamaster/index.js";
+import { PrismaClient as PrismaHrPortal } from "../generated/prisma-hrportal/index.js";
 
 declare global {
   var prisma: PrismaSAR | undefined;
   var prismaSC: PrismaSC | undefined;
   var prismaLdap: PrismaLdap | undefined;
   var prismaTmmin: PrismaTmmin | undefined;
+  var prismaDataMaster: PrismaDataMaster | undefined;
+  var prismaHrPortal: PrismaHrPortal | undefined;
 }
 
 export const prisma =
@@ -36,10 +40,24 @@ export const prismaTmmin =
   new PrismaTmmin({
     log: ["error"],
   });
+  
+export const prismaDataMaster =
+  global.prismaDataMaster ||
+  new PrismaDataMaster({
+    log: ["error"],
+  });
+
+export const prismaHrPortal =
+  global.prismaHrPortal ||
+  new PrismaHrPortal({
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
   global.prismaSC = prismaSC;
   global.prismaLdap = prismaLdap;
   global.prismaTmmin = prismaTmmin;
+  global.prismaDataMaster = prismaDataMaster;
+  global.prismaHrPortal = prismaHrPortal;
 }
